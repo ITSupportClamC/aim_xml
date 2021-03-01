aa61248e5098bfd047ff1e71079a9684d67139aa
Steven Zhang <steven.zhang@CLAMC.COM.HK>
2021-03-01 19:19:11 +0800

    doing dummy rerate now

---
+++ dummy_rerate.py
@@ -0,0 +1,87 @@
+# coding=utf-8
+#
+# Read Repo trade file, and create dummy rerate file from it.
+# 
+from aim_xml.repo_datastore import getRepoTradeFromFile
+from toolz.functoolz import compose
+from functools import partial
+import logging
+logger = logging.getLogger(__name__)
+
+
+
+"""
+	1 Get all repo trades of type OPEN;
+	2 Create string of text (variable rate record) for each trade;
+	3 Save all text into text file.
+"""
+def getRepoOpenTrades(file):
+	"""
+	[String] file => [Iterable] trades
+	"""
+	"""
+	get all trades
+	filter only open repo trade
+	"""
+	logger.debug('getRepoOpenTrades(): {0}'.format(file))
+
+	repoOpenTrade = lambda t: \
+		t['TransactionType'] in ('ReverseRepo_InsertUpdate', 'Repo_InsertUpdate') \
+		and 'LoanAmount' in t and t['ActualSettleDate'] == 'CALC'
+
+
+	return \
+	compose(
+		partial(filter, repoOpenTrade)
+	  , getRepoTradeFromFile
+	)(file)
+
+
+
+def toXMLString(trade):
+	"""
+	[Dictionary] repo trade of type OPEN => [String] text
+	"""
+	return \
+	'<VariableRateRecord>\n' + '<Loan>UserTranId1={0}</Loan>\n'.format(trade['UserTranId1']) + \
+	'<RateTable>\n<CommitType>Insert</CommitType>\n' + \
+	'<RateDate>{0}</RateDate>\n'.format(trade['SettleDate']) + \
+	'<Rate>{0}</Rate>\n'.format(trade['Coupon']) + \
+	'</RateTable>\n</VariableRateRecord>'
+
+
+
+def toFile(outputDir, file, text):
+	"""
+	[String] output directory, [String] output file, [String] text 
+		=> [String] output file (with full path)
+
+	side effect: create an XML file in the output directory
+	"""
+	return ''
+
+
+
+if __name__ == "__main__":
+	import logging.config
+	logging.config.fileConfig('logging.config', disable_existing_loggers=False)
+	
+	import argparse
+	parser = argparse.ArgumentParser(description='Create dummy repo rerate files')
+	parser.add_argument('file', metavar='file', type=str, help="repo trade XML file")
+
+	"""
+	To test the program, put a repo trade file (without headers) into
+	the local directory and run:
+
+		$ python dummy_rerate.py <repo trade file>
+
+	"""
+	# get output directory from input file
+	# get output file name from input file
+	compose(
+		print
+	  , lambda L: '\n'.join(L)
+	  , partial(map, toXMLString)
+	  , getRepoOpenTrades
+	)(parser.parse_args().file)
